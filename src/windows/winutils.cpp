b64a5aa1fc5afe4053edbc5f911d618e1523fd2a
AGBDev <nils19052006@gmail.com>
2024-12-18 19:24:43 +0100

    h

--- src/windows/winutils.cpp
+++ src/windows/winutils.cpp
@@ -26,22 +26,29 @@ int quick_messagebox(const std::string &text, unsigned long types)
     return quick_messagebox(text, global_window_title, types);
 }
 
-std::vector<std::string> error_code_messages{ "Failed to open std::fstream" };
+std::vector<std::string> error_code_messages { "Failed to open std::fstream" };
 
+constexpr std::string unknown_error = "Unknown Error";
 static std::string get_error_code_message(size_t error_code) 
 {
     if (error_code_messages.size() - 1 < error_code)
-        return "Unknown error";
+        return unknown_error;
     for (size_t i = 0; i < error_code_messages.size(); i++)
     {
-        if (error_code == i) return error_code_messages[i];
+        if (error_code == i) 
+            return error_code_messages[i];
     }
-    return "Unknown error";
+    return unknown_error;
 }
 
 int display_error_message(int error_code, const std::string &source, const std::string &extra_info)
 {
-    std::string message{"An error occured in AGB-Library (agb.dll)\n\n"};
+    std::string message{"An error occured in AGB-Library (libagb.dll)\n"};
+    message.append("Affected Software: ");
+    message.append(get_global_window_title());
+    message.append("\n");
+    message.append("\n");
+    message.append("\n");
     message.append("Library Version: ");
     message.append(std::to_string(get_lib_version()));
     message.append("\n");
@@ -54,7 +61,12 @@ int display_error_message(int error_code, const std::string &source, const std::
     message.append("Source: ");
     message.append(source);
     message.append("\n");
+    message.append("\n");
     message.append("Additional Info: ");
     message.append(extra_info);
-    return quick_messagebox(message, "AGB-Library", wind_error_ok);
+
+    std::string title = "AGB-Libary";
+    title.append(" in ");
+    title.append(get_global_window_title());
+    return quick_messagebox(message, title, wind_error_ok);
 }
